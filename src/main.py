9a1fad145d5dd701b8739d06ba7b850dfadbc1de
subsir <tiancaizhangdaxian@sjtu.edu.cn>
2024-10-28 20:29:50 +0800

    这个才是

--- src/main.py
+++ src/main.py
@@ -8,21 +8,21 @@ import pre_llvm
 
 code = sys.stdin.read()
 code2 = semantic.main(code)
-# try:
-code2_5 = pre_llvm.main(code2)
-with open("semantic.txt", "w") as f:
-    f.write(code2_5)
-code3 = llvm.main(code2_5)
-with open("llvm.ll", "w") as f:
-    f.write(code3)
-code3 = llvm_optim.main(code3)
-with open("llvm_optim.ll", "w") as f:
-    f.write(code3)
-code4 = asm.main(code3)
-with open("test.s", "w") as f:
-    f.write(code4)
-with open("src/builtin.s", "r") as f:
-    print(f.read())
-print(code4)
-# except:
-#     pass
+try:
+    code2_5 = pre_llvm.main(code2)
+    with open("semantic.txt", "w") as f:
+        f.write(code2_5)
+    code3 = llvm.main(code2_5)
+    with open("llvm.ll", "w") as f:
+        f.write(code3)
+    code3 = llvm_optim.main(code3)
+    with open("llvm_optim.ll", "w") as f:
+        f.write(code3)
+    code4 = asm.main(code3)
+    with open("test.s", "w") as f:
+        f.write(code4)
+    with open("src/builtin.s", "r") as f:
+        print(f.read())
+    print(code4)
+except:
+    pass
